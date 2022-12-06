34902d3fab3738480ad8ba68b2b1b447440de2df
Volantis <putzstuck@gmail.com>
2022-12-05 22:00:54 -0500

    added header and updated navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,40 +3,44 @@ import '../styles/Navbar.css';
 
 function navbar({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >Work
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >Resume
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-        >About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >Inquire
-        </a>
-      </li>
-    </ul>
+    <header>
+      <h1>Craig Putzstuck<span>FULL STACK WEB DEVELOPER</span></h1>
+      
+      <ul className="nav">
+        <li className="nav-item">
+          <a
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          >Work
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >Resume
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#blog"
+            onClick={() => handlePageChange('Blog')}
+            className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+          >About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          >Inquire
+          </a>
+        </li>
+      </ul>
+    </header>
   );
 }
 
