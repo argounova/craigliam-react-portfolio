15275ce172bedbf762810bbe25116bae7f17f4c2
Volantis <putzstuck@gmail.com>
2022-12-06 23:25:33 -0500

    revised about section paragraphs

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,20 +2,29 @@ import React from 'react';
 import '../../styles/About.css';
 import Image from '../../images/my-profile-photo.jpg';
 
+const style = {
+  para: {
+    padding: '2%'
+  }
+};
 
 export default function About() {
   return (
     <div class="contentDiv">
       <h1>Craig Putzstuck</h1>
-      <h3>WEB DEVELOPER</h3>
+      <h3>MERN FULL STACK WEB DEVELOPER</h3>
       <div className="wrapText">
         <div>
-          <img src={Image} alt="Profile Picture"/>
+          <img src={Image} style={style.para}/>
         </div>
-          <p>
-           Welcome and thanks for stopping by! Here you will find examples of my most recent work, but first, allow me to introduce myself. I am a full stack JavaScript web developer and simply put, I build web applications. When I'm not in front of the computer, you can typically find me in the garage trying to make one of my cars shinier or faster than it was the day before. I have an unbridled passion for motorsports and in my second life I am actively pursuing an amateur racing career.
+          <p style={style.para}>
+            I have always maintained a curious mind and a desire to see projects through to fruition. While cars and racing have held my interests hostage for the most part, architecture, 3D modeling and wood working have also held sway. It was somewhat natural then to transition into web development as technology progressed and the realization that the architecture of the future lies in code. As an analytical individual planning and design are ingrained in my DNA which has allowed JavaScript to become a very rewarding aspect of my life.
           <br/>
-           Cars and computers go hand in hand these days and in the not-so-distant-future, cars will be computers. As a developer with a wealth of automotive knowledge, I seek to position myself at the forefront of such an intersection. I hope to work with innovative, free-thinking, dedicated and diverse individuals who hold progress and global awareness as some of their core values. Ready to get started? I'd love to hear from you.
+          <br/>
+            My overall focus is on the MERN stack, however, there are several supporting technologies that have really garnered my attention. Handlebars.js, Bootstrap, jQuery, third-party API's and Apollo GraphQL to name a few. Understanding the intersection of front-end and back-end development has proven to be fascinating and although tedious and challenging at times (most times if I'm honest!) is absolutely integral to a proper web application. 
+          <br/>
+          <br/>
+            As mentioned previously, motorsport is a serious component to my life. It is something I have been around for as long as I can remember. With age I gain a greater appreciation for the history surrounding racing and for the inumerous individuals that have made it what it is today. Technology also plays an undeniable and all-consuming role in motorsport as racecars move towards sustainable fuels and electric power. In the not-so-distant-future, cars will be computers. As a developer with a wealth of automotive knowledge, I seek to position myself at the forefront of such an intersection. I hope to work with innovative, free-thinking, dedicated and diverse individuals who hold progress and global awareness as some of their core values. Ready to get started? I'd love to hear from you.
           </p>
       </div>
     </div>
